60c2ce8684111387d8d86b920177e0278b4dbf6e
Radhika Arora <aradhika880@gmail.com>
2022-11-12 02:07:59 +0530

    Update Set Bit.c

--- Set Bit.c
+++ Set Bit.c
@@ -1,5 +1,5 @@
-//This function deals with setting the bit. Setting bit means if we are given a specific position then in that case, we have to set 1 there. And unsetting abit means, if we are given a position, we need to unset that bit to 0. This is also done likely to the get bit function, the only differeence is instead of perfoming & operation here, we need to perform | operation here.
-void set_bit(int n, int pos){
+//This function deals with setting the bit. Setting bit means if we are given a specific position then in that case, we have to set 1 there. And unsetting a bit means, if we are given a position, we need to unset that bit to 0. This is also done likely to the get_bit function, the only difference is instead of performing & operation here, we need to perform | operation here.
+int set_bit(int n, int pos){
     return (n  | (1<<pos));
 }
 
